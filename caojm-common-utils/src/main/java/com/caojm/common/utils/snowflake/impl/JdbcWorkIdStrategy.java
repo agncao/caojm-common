bdee9f71d84009f2bd31293fea08fe621fb6fa2c
caojianmin <caojianmin@jd.com>
2018-02-22 17:26:04 +0800

    自增长id

---
+++ caojm-common-utils/src/main/java/com/caojm/common/utils/snowflake/impl/JdbcWorkIdStrategy.java
@@ -0,0 +1,24 @@
+package com.caojm.common.utils.snowflake.impl;
+
+import com.caojm.common.utils.snowflake.SequenceUtil;
+import com.caojm.common.utils.snowflake.WorkerIdStrategy;
+import org.springframework.stereotype.Component;
+
+/**
+ * 利用数据库的自增长机制来得到sequence
+ *
+ * @author <a href="mailto:caojianmin@jd.com">caojianmin</a>
+ * @create 2018/02/22 17:23
+ */
+@Component
+public class JdbcWorkIdStrategy extends SequenceUtil {
+    @Override
+    public long nextId(WorkerIdStrategy workerIdStrategy) {
+        return 0;
+    }
+
+    @Override
+    public long nextId(long workId) {
+        return 0;
+    }
+}
